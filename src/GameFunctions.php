e9d31280741a49187ea2d31c0a515e5f7121a48d
Urban Etter <uetter@gmail.com>
2017-09-22 20:54:48 +0200

    Added team game functions

--- src/GameFunctions.php
+++ src/GameFunctions.php
@@ -4,7 +4,11 @@ namespace Jass\Game;
 
 use Jass\Entity\Game;
 use Jass\Entity\Player;
+use Jass\Entity\Trick;
+use function Jass\Hand\last;
 use Jass\Style\Style;
+use function Jass\Trick\points;
+use function Jass\Trick\winner;
 
 function isFinished(Game $game)
 {
@@ -27,4 +31,55 @@ function isReady(Game $game)
         && $game->currentPlayer
         && $game->currentPlayer instanceof Player
     ;
+}
+
+function teams(Game $game)
+{
+    return [$game->players[0]->team, $game->players[1]->team];
+}
+
+function teamMatched(string $team, Game $game)
+{
+    if (!isFinished($game)) {
+        return false;
+    }
+
+    $tricksWon = array_filter($game->playedTricks, function(Trick $trick) use ($team, $game) {
+        return winner($trick, $game->style->orderFunction())->team == $team;
+    });
+
+    return $tricksWon == Game::NUMBER_OF_CARDS;
+}
+
+function teamWonLastTrick(string $team, Game $game)
+{
+    if (!isFinished($game)) {
+        return false;
+    }
+
+    /** @var Trick $lastTrick */
+    $lastTrick = last($game->playedTricks);
+    return winner($lastTrick, $game->style->orderFunction())->team == $team;
+}
+
+function teamPoints(string $team, Game $game)
+{
+    $points = array_reduce($game->playedTricks, function($sum, Trick $trick) use ($game, $team) {
+        if (winner($trick, $game->style->orderFunction())->team == $team) {
+            $sum += points($trick, $game->style->pointFunction());
+        }
+        return $sum;
+    }, 0);
+
+    // winning last trick adds 5 points
+    if (teamWonLastTrick($team, $game)) {
+        $points += 5;
+    }
+
+    // winning all tricks adds 100 points
+    if (teamMatched($team, $game)) {
+        $points += 100;
+    }
+
+    return $points;
 }
